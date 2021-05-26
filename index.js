098ed03cd8bd6835cc4fd2f2fffa21ce6bf8182f
Christopher DiStasio <cadistasio@gmail.com>
2021-05-26 14:17:23 -0500

    Completes all tasks

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
+let changeHeader = document.querySelector("#page-header");
 
-// Select some elements...
+changeHeader.style.textAlign = "left";
+
+let borders = document.querySelectorAll(".dog-image");
+
+for (let i = 0; i < borders.length; i++) {
+  borders[i].style.borderRadius = "50px"
+  borders[i].style.border = "10px solid"
+  borders[i].style.transform = "rotate(180deg)"
+}
+
+let leftNames = document.querySelectorAll(".dog-name")
+
+leftNames.forEach(n => {
+  n.style.textAlign = "left"
+})
+
+let footerColor = document.querySelector(".footer")
+
+footerColor.style.color = "purple"
+
